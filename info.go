2e416e775816a95d833de8eef450374bfc8a3ef2
Alexey Rusov <rolic402@mail.ru>
2020-11-23 16:52:56 +0300

    +/status

--- info.go
+++ info.go
@@ -199,6 +199,7 @@ func (h *HTTP) initInfo() {
 		"/profiler-disable": "Disable profiler. Parameters: -",
 		"/profiler-enable":  "Enable profiler. Parameters: -",
 		"/set-log-level":    "Temporarily change log level. Parameters: level=<level>",
+		"/status":           "Application current status. Parameters: -",
 	})
 }
 
