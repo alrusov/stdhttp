800d2035e53d9e960c80c220d208e1a70a8876c6
Alexey Rusov <rolic402@mail.ru>
2022-08-18 18:39:15 +0300

    favicon size changed again

--- pages.go
+++ pages.go
@@ -19,7 +19,7 @@ var (
 `
 
 	rootPage = htmlTop + `
-		<h4><img src="/favicon.ico" style="width: 32px; height: 32px; position: relative; top: 2px;" alt="" />&nbsp;<em>{{$.Name}} [{{$.App}} {{$.Version}}{{if $.Tags}}&nbsp;{{$.Tags}}{{end}}]</em></h4>
+		<h4><img src="/favicon.ico" style="width: 64px; height: 64px;" alt="" />&nbsp;<em>{{$.Name}} [{{$.App}} {{$.Version}}{{if $.Tags}}&nbsp;{{$.Tags}}{{end}}]</em></h4>
 
 		{{if $.ErrMsg}}<p><strong class="attention">{{$.ErrMsg}}</strong></p>{{end}}
 
