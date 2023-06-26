9d2f07498c570b311cd589d81f154f0750f62d19
KozlovP <KozlovP@terralink.ru>
2023-06-26 16:36:16 +0300

    code: Add extended request

--- request.go
+++ request.go
@@ -170,3 +170,126 @@ func Request(method string, uri string, timeout time.Duration, opts misc.StringM
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
+
+// RequestEx --
+func RequestEx(method string, uri string, timeout time.Duration, opts map[string][]string, extraHeaders map[string][]string, data []byte) (*bytes.Buffer, *http.Response, error) {
+	params := url.Values{}
+
+	if data == nil {
+		data = make([]byte, 0)
+	}
+
+	withGzip := gzipRecommended(data)
+	skipTLSverification := false
+	user := ""
+	password := ""
+
+	for k, values := range opts {
+		v := values[0]
+		if strings.HasPrefix(k, ".") {
+			switch k {
+			case RequestOptionGzip:
+				withGzip = withGzip && parseBoolOption(v)
+			case RequestOptionSkipTLSVerification:
+				skipTLSverification = parseBoolOption(v)
+			case RequestOptionBasicAuthUser:
+				user = v
+			case RequestOptionBasicAuthPassword:
+				password = v
+			}
+			continue
+		}
+		for _, v := range values {
+			params.Add(k, v)
+		}
+
+	}
+
+	if withGzip {
+		b, err := misc.GzipPack(bytes.NewReader(data))
+		if err != nil {
+			return nil, nil, err
+		}
+		data = b.Bytes()
+	}
+
+	req, err := http.NewRequest(method, uri, bytes.NewReader(data))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if withGzip {
+		req.Header.Set("Content-Encoding", "gzip")
+	}
+
+	for n, values := range extraHeaders {
+		for _, v := range values {
+			req.Header.Set(n, v)
+		}
+	}
+
+	if _, exists := extraHeaders["Accept-Encoding"]; !exists {
+		req.Header.Set("Accept-Encoding", "gzip")
+	}
+
+	if user != "" || password != "" {
+		req.SetBasicAuth(user, password)
+	}
+
+	req.URL.RawQuery = params.Encode()
+
+	if timeout == 0 {
+		timeout = config.ClientDefaultTimeout.D()
+	}
+
+	var tr *http.Transport
+	switch req.URL.Scheme {
+	case "http", "https":
+		tr = &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: skipTLSverification,
+			},
+		}
+	case "unix":
+		req.URL.Scheme = "http"
+		tr = &http.Transport{
+			Dial: (&unixSocketDialer{
+				net.Dialer{
+					Timeout:   timeout,
+					KeepAlive: timeout,
+				},
+			}).Dial,
+		}
+	default:
+		return nil, nil, fmt.Errorf(`unknown scheme "%s"`, req.URL.Scheme)
+	}
+
+	c := &http.Client{
+		Timeout:   timeout,
+		Transport: tr,
+	}
+
+	resp, err := c.Do(req)
+	tr.CloseIdleConnections()
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	bodyBuf, _, err := ReadData(resp.Header, resp.Body)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	if resp.StatusCode/100 != 2 {
+		return bodyBuf, resp, errors.New("Status code " + strconv.Itoa(resp.StatusCode))
+	}
+
+	return bodyBuf, resp, nil
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
